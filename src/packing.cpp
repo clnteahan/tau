6f5602a4df25f458382220839b8afccd42d2f602
Colin Teahan <colin@teahan.us>
2026-02-20 05:58:39 -0500

    minor update [may not work]

--- src/packing.cpp
+++ src/packing.cpp
@@ -1,10 +1,10 @@
 #include "packing.h"
 
-packing_list::packing_list(const std::string &dir, const std::string& name) {
+packer::packer(const std::string &dir, const std::string& name) {
 	this->dir = dir;
 	this->clist = new compression_list(dir + "/" + name + ".tar.xz");
 }
 
-packing_list::~packing_list() {
+packer::~packer() {
 	delete this->clist;
 }
